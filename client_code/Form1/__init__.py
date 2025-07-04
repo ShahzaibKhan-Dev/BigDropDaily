03d2a4d8379a8ac8d1e04a28dc26868f89b5dcb4
shahzaibwebfreelancer@gmail.com <shahzaibwebfreelancer@gmail.com>
2025-07-04 16:04:43 +0000

    Created Big Drop Daily.png; Edited settings, theme/parameters.yaml and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import plotly.graph_objects as go
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,19 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def Username(self, **event_args):
+    """This method is called when the TextBox loses focus"""
+
+  def text_box_3_lost_focus(self, **event_args):
+    """This method is called when the TextBox loses focus"""
+    pass
+
+  def rich_text_1_show(self, **event_args):
+    """This method is called when the RichText is shown on the screen"""
+    pass
+
+  def text_box_4_show(self, **event_args):
+    """This method is called when the TextBox is shown on the screen"""
+    pass
+     
